e9de303976fc95bffc88430ad2432f455155e1ed
Lucas Santos <lucas.nepomuceno1999@gmail.com>
2020-10-24 15:43:39 -0300

    adding object version to each kind, in buildSrc...

--- buildSrc/src/main/java/Libraries.kt
+++ buildSrc/src/main/java/Libraries.kt
@@ -1,45 +1,74 @@
 class Libraries {
+
     object BuildScript {
-        const val gradle = "com.android.tools.build:gradle:4.1.0"
-        const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
+        const val gradle = "com.android.tools.build:gradle:${Core.Versions.kotlin}"
+        const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Core.Versions.kotlin}"
         const val hiltAndroidGradlePlugin =
-            "com.google.dagger:hilt-android-gradle-plugin:2.28-alpha"
+            "com.google.dagger:hilt-android-gradle-plugin:${DaggerHilt.Versions.daggerHilt}"
     }
 
-
     object Ui {
+
+        object Versions{
+            const val navigation = "2.3.1"
+            const val material = "1.2.1"
+            const val constraintLayout = "2.0.2"
+            const val viewModel = "2.3.0-beta01"
+        }
+
         object Navigation {
-            const val navigationUi = "androidx.navigation:navigation-ui-ktx:2.3.1"
-            const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:2.3.1"
-            const val navigationSafeArgs = "androidx.navigation:navigation-safe-args-generator:2.3.1"
-            const val navigationSafeArgsPlugin = "androidx.navigation:navigation-safe-args-gradle-plugin:2.3.1"
+            const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+            const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+            const val navigationSafeArgs = "androidx.navigation:navigation-safe-args-generator:${Versions.navigation}"
+            const val navigationSafeArgsPlugin = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
         }
 
         object ViewModel {
-            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-beta01"
+            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.viewModel}"
         }
 
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.2"
-        const val material = "com.google.android.material:material:1.2.1"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+        const val material = "com.google.android.material:material:${Versions.material}"
     }
 
     object Test {
-        const val junit = "junit:junit:4.13.1"
-        const val extJunit = "androidx.test.ext:junit:1.1.2"
-        const val espresso = "androidx.test.espresso:espresso-core:3.3.0"
+
+        object Versions{
+            const val junit = "4.13.1"
+            const val extJunit = "1.1.2"
+            const val expresso = "3.3.0"
+        }
+
+        const val junit = "junit:junit:${Versions.junit}"
+        const val extJunit = "androidx.test.ext:junit:${Versions.extJunit}"
+        const val espresso = "androidx.test.espresso:espresso-core:${Versions.expresso}"
+
     }
 
     object Core {
-        const val appCompat = "androidx.appcompat:appcompat:1.2.0"
-        const val coreKtx = "androidx.core:core-ktx:1.3.2"
-        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:1.4.10"
-        const val stdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10"
+
+        object Versions{
+            const val kotlin = "1.4.10"
+            const val coreKtx = "1.3.2"
+            const val appCompat = "1.2.0"
+        }
+
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+        const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+        const val stdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
     }
 
-    object DaggerHilt {
-        const val hilt = "com.google.dagger:hilt-android:2.28-alpha"
-        const val hiltCompiler = "com.google.dagger:hilt-android-compiler:2.28-alpha"
-        const val viewModelCompiler = "androidx.hilt:hilt-compiler:1.0.0-alpha02"
-        const val viewModel = "androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha02"
+    object DaggerHilt{
+
+        object Versions{
+            const val daggerHilt = "2.28-alpha"
+            const val viewModelHilt = "1.0.0-alpha02"
+        }
+
+        const val hilt = "com.google.dagger:hilt-android:${Versions.daggerHilt}"
+        const val hiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.daggerHilt}"
+        const val viewModelCompiler = "androidx.hilt:hilt-compiler:${Versions.viewModelHilt}"
+        const val viewModel = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.viewModelHilt}"
     }
 }
